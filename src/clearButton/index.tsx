866e791a53a737bf895660f254d003d69e4231be
Marcela Fernanda <marcela.oliveira@algoritme.com.br>
2021-07-09 11:32:12 -0300

    Add new props to clearButton

--- src/clearButton/index.tsx
+++ src/clearButton/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import type {TColors} from '../config/colors';
-import {Container, Text} from './styles';
+import React from "react";
+import type { TColors } from "../config/colors";
+import { Container, Text } from "./styles";
 
 interface IProps {
   m?: string;
@@ -8,15 +8,17 @@ interface IProps {
   text2: string;
   textColor1?: TColors;
   textColor2?: TColors;
+  onPress?(): void;
 }
 
 const ClearButton = (props: IProps) => {
   return (
-    <Container m={props.m}>
-      <Text textColor={props.textColor1 || 'titles'}>{props.text1} </Text>
+    <Container m={props.m} onPress={props.onPress}>
+      <Text textColor={props.textColor1 || "titles"}>{props.text1} </Text>
       <Text
-        style={{textDecorationLine: 'underline'}}
-        textColor={props.textColor2 || 'primary'}>
+        style={{ textDecorationLine: "underline" }}
+        textColor={props.textColor2 || "primary"}
+      >
         {props.text2}
       </Text>
     </Container>

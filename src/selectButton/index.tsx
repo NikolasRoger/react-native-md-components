039f36aa6190a82693fb296530ae89f84d14c7dc
Marcela Fernanda <marcela.oliveira@algoritme.com.br>
2021-07-13 16:35:49 -0300

    Adding prop to selectButton

--- src/selectButton/index.tsx
+++ src/selectButton/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import {Container, Text} from './styles';
-import Icon from 'react-native-vector-icons/Ionicons';
-import Colors from '../config/colors';
-import MdIcon from '../mdIcon';
+import React from "react";
+import { Container, Text } from "./styles";
+import Icon from "react-native-vector-icons/Ionicons";
+import Colors from "../config/colors";
+import MdIcon from "../mdIcon";
 
 interface IItems {
   name: string;
@@ -11,6 +11,7 @@ interface IItems {
 
 interface IProps {
   items: IItems[];
+  onPress?(): any;
 }
 
 const SelectButton = (props: IProps) => {
@@ -18,9 +19,10 @@ const SelectButton = (props: IProps) => {
     <>
       {props.items.map((item, index) => (
         <Container
+          onPress={props.onPress}
           key={index}
           style={{
-            shadowColor: '#000',
+            shadowColor: "#000",
             shadowOffset: {
               width: 0,
               height: 2,
@@ -29,12 +31,13 @@ const SelectButton = (props: IProps) => {
             shadowRadius: 3.84,
 
             elevation: 5,
-          }}>
+          }}
+        >
           {item.showIcon && <MdIcon image="couponIcon" size="24px" />}
           <Text>{item.name}</Text>
           <Icon
             name="chevron-forward-outline"
-            color={Colors['titles']}
+            color={Colors["titles"]}
             size={28}
           />
         </Container>

6605c86a57001cb7cd96533f420921adf4290ea0
Nikolas Roger <nick@algoritme.com.br>
2021-08-17 14:49:19 -0300

    ADD forwardref

--- src/input/index.tsx
+++ src/input/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { TextInputProps } from 'react-native';
+import type { TextInput, TextInputProps } from 'react-native';
 import type {TColors} from '../config/colors';
 import {StyledInput, Label, Container} from './styles';
 
@@ -11,15 +11,16 @@ interface IProps extends TextInputProps {
   m?: string;
 }
 
-const Input = ({m, label, textColor, type, ...rest}: IProps) => {
+const Input = React.forwardRef(({m, label, textColor, type, ...rest}: IProps, ref?: React.Ref<TextInput>) => {
   return (
     <Container m={m}>
       {label && <Label textColor={textColor}>{label}</Label>}
       <StyledInput
         type={type}
+        ref={ref}
         {...rest}></StyledInput>
     </Container>
   );
-};
+});
 
 export default Input;

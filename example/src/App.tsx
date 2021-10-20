9d5223e3c71fe22738189a0c2133e3f9927793a1
Tainan Nery <tainan.nery@algoritme.com.br>
2021-10-20 14:43:34 -0300

    Example updated

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,41 @@
 import * as React from 'react';
-import {  View, Text } from 'react-native';
-import { HorizontalCategories } from 'react-native-md-components'
+import {  View } from 'react-native';
+import { Carousel, AddressSelect, HorizontalCategories } from 'react-native-md-components'
 
-const categories = [
+const data = [
   {
-    title: 'teste'
+    id: 1,
+    title: 'Serviços de Tecnologia'
+  },
+  {
+    id: 2,
+    title: 'Vestuário e Calçados'
+  },
+  {
+    id: 3,
+    title: 'Teste'
+  },
+  {
+    id: 4,
+    title: 'Teste'
+  },
+  {
+    id: 5,
+    title: 'Teste'
+  },
+  {
+    id: 6,
+    title: 'Teste'
   }
 ]
 
 export default function App() {
-
   return (
     <View>
-      <Text>Result</Text>
-      <HorizontalCategories items={categories} categorie="Teste"/>
+      <Carousel/>
+      <AddressSelect address="Rua Odilardo Silva, 1951, Centro, Macapá"/>
+      <HorizontalCategories items={data} categorie="Serviços"/>
     </View>
   );
 }
+// ${defaultAddress.street}, ${defaultAddress.number}, ${defaultAddress.neighborhood}, ${defaultAddress.city}
